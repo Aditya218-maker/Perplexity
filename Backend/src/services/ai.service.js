35dd79eb65938099081246c4a17a790e69012541
Aditya218-maker <aditya777anand@gmail.com>
2026-08-16 09:34:50 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Included Vite configuration for React and Tailwind CSS
    - Created main application structure with App component and styles
    - Added SVG assets for React and Vite logos
    - Implemented basic CSS styles for layout and animations
    - Set up entry point in main.jsx to render the App component

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,16 +1,12 @@
 import { ChatGoogle } from "@langchain/google";
-
-// Latest model setup
 const model = new ChatGoogle({
-    model: "gemini-flash-latest", // ⚡ Latest & Superfast model
-    apiKey: process.env.GEMINI_API_KEY
+    model: "gemini-flash-latest", 
+    apiKey: process.env.GOOGLE_API_KEY
 });
 
 export async function testAI() {
     try {
         const response = await model.invoke("what is Ai explain in 10 words?");
-        
-        // 🎯 LangChain mein output `content` property mein hota hai!
         console.log("AI Response:", response.content);
     } catch (error) {
         console.error("AI Fetch Error:", error);
