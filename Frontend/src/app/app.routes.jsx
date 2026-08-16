7e07e2f31ff067c9f1cd7c105635952ca56cc581
Aditya218-maker <aditya777anand@gmail.com>
2026-08-16 10:04:01 +0530

    feat: implement Login and Register components with form handling and routing

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter } from "react-router"
+import Login from "../features/auth/pages/Login"
+import Register from "../features/auth/pages/Register"
 
 export const router = createBrowserRouter([
     {
         path: "/login",
-        element: <h1>Login</h1>
+        element: <Login />
     },
     {
-
+        path: "/register",
+        element: <Register />
     },
     {
 
